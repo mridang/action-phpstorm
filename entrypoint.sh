f28727cfd83225018b3c4d08fc9573d1cf0c61e9
Mridang Agarwalla <mridang@nosto.com>
2020-05-17 22:50:10 +0530

    Updated workflow

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo $2
 echo $3
 echo $4
 /opt/ide/bin/inspect.sh $1 $2 $3 -d $1 -$4
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs xsltproc problems.xslt
+find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs xsltproc /problems.xslt
