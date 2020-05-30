6b03de45da9ef1b5c613e3a0cdbe7c8a163f9bfd
Mridang Agarwalla <mridang@nosto.com>
2020-05-30 13:48:36 +0530

    Test commit for trigger CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,11 @@ echo $6 | awk 'BEGIN{RS=","} {print}' | xargs -I{} rm -f "$3/{}"
 # Remove all the references to GITHUB_WORKSPACE in all the XML files. The
 # insection results have file paths in the format.
 # file://$PROJECT_DIR$/$GITHUB_WORKSPACE
-cat output/PhpUndefinedFieldInspection.xml
+#cat output/PhpUndefinedFieldInspection.xml
 # Notice that $GITHUB_WORKSPACE is a variable, while $PROJECT_DIR$ is not
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs sed -i "s/$GITHUB_WORKSPACE//g"
+#find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs sed -i "s/$GITHUB_WORKSPACE//g"
 # Now we'll remove the file://$PROJECT_DIR$ references
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs sed -i 's/file:\\$PROJECT_DIR$//g'
-cat output/PhpUndefinedFieldInspection.xml
+#find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs sed -i 's/file:\\$PROJECT_DIR$//g'
+#cat output/PhpUndefinedFieldInspection.xml
 # Now to iterate all the XML files, transform them and then print them.
 find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs xsltproc /problems.xslt
--- problems.xslt
+++ problems.xslt
@@ -10,7 +10,7 @@
     <xsl:text>::</xsl:text>
     <xsl:text>warning </xsl:text>
     <xsl:text>file=</xsl:text>
-    <xsl:value-of select="str:replace (file, 'file://$PROJECT_DIR$', '')"/>
+    <xsl:value-of select="file"/>
     <xsl:text>,line=</xsl:text>
     <xsl:value-of select="line"/>
     <xsl:text>::</xsl:text>
