2189dbd8cfd5cd0b6e1e455e3d8dfccde1b4a812
Mridang Agarwalla <mridang@nosto.com>
2020-05-30 14:21:23 +0530

    Cat XSLT

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,25 +8,35 @@ if [ "$5" != "default" ]; then
   echo "-Didea.analyze.scope=$5" >> /opt/ide/bin/phpstorm64.vmoptions
 fi
 
-/opt/ide/bin/inspect.sh $1 $2 $3 -d $1 -$4
-echo $6 | awk 'BEGIN{RS=","} {print}' | xargs -I{} rm -f "$3/{}"
+# Run the inspection with the parameters provided. This script is a real pain to
+# work with so be cautious when editing the order of the parameters.
+echo "Running inspections"
+/opt/ide/bin/inspect.sh "$1" "$2" "$3" -d "$1" "-$4"
 
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs xsltproc /files.xslt | sort | uniq
+echo "Cleaning path references"
+# The default run generates one file for each inspection type. We want to remove
+# all the not needed ones. The user provides a comma delimited list of files to
+# ignore and we simply explode and remove those
+echo "$6" | awk 'BEGIN{RS=","} {print}' | xargs -I{} rm -f "$3/{}"
+
+find "$3" -name '*.xml' ! -name '.descriptions.xml' | xargs -0 xsltproc /files.xslt | sort | uniq
 
 # Now we'll remove the $PROJECT_DIR$ references
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs sed -i 's|$PROJECT_DIR$||g'
+find "$3" -name '*.xml' ! -name '.descriptions.xml' | xargs -0 sed -i 's|$PROJECT_DIR$||g'
 
 # Now we'll remove the file:// references
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs sed -i 's|file://||g'
+find "$3" -name '*.xml' ! -name '.descriptions.xml' | xargs -0 sed -i 's|file://||g'
 
 # Remove all the references to GITHUB_WORKSPACE in all the XML files. The
-# insection results have file paths in the format.
-# file://$PROJECT_DIR$/$GITHUB_WORKSPACE
+# inspection results have file paths in the format.
+# file://$PROJECT_DIR$/$GITHUB_WORKSPACE or file://$GITHUB_WORKSPACE
 # Notice that $GITHUB_WORKSPACE is a variable, while $PROJECT_DIR$ is not
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs sed -i "s|$GITHUB_WORKSPACE||g"
+find "$3" -name '*.xml' ! -name '.descriptions.xml' | xargs -0 sed -i "s|$GITHUB_WORKSPACE||g"
 
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs xsltproc /files.xslt | sort | uniq
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs xsltproc /files.xslt | sort | uniq | xargs -I{} test -e {}
+echo "Sanity check file paths"
+find "$3" -name '*.xml' ! -name '.descriptions.xml' | xargs -0 xsltproc /files.xslt | sort | uniq
+find "$3" -name '*.xml' ! -name '.descriptions.xml' | xargs -0 xsltproc /files.xslt | sort | uniq | xargs -I{} test -e {}
 
+echo "Parsing problems and reporting annotations"
 # Now to iterate all the XML files, transform them and then print them.
-find $3 -name '*.xml' ! -name '.descriptions.xml' | xargs xsltproc /problems.xslt
+find "$3" -name '*.xml' ! -name '.descriptions.xml' | xargs -0 xsltproc /problems.xslt
